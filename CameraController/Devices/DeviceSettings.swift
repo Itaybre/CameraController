41470a3a71783c55c86fe26152068d9f347e33f4
Itay <itay@itaysoft.com>
2020-08-07 01:10:09 -0300

    Add DeviceSettings struct

---
+++ CameraController/Devices/DeviceSettings.swift
@@ -0,0 +1,30 @@
+//
+//  DeviceSettings.swift
+//  CameraController
+//
+//  Created by Itay Brenner on 8/7/20.
+//  Copyright © 2020 Itaysoft. All rights reserved.
+//
+
+import Foundation
+
+struct DeviceSettings {
+    var name: String
+
+    let exposureMode: Int
+    let exposureTime: Float
+    let gain: Float
+    let brightness: Float
+    let contrast: Float
+    let saturation: Float
+    let sharpness: Float
+    let whiteBalanceAuto: Bool
+    let whiteBalance: Float
+    let powerline: Float
+    let backlightCompensation: Float
+    let zoom: Float
+    let pan: Float
+    let tilt: Float
+    let focusAuto: Bool
+    let focus: Float
+}
