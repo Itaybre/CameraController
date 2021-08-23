4103f272bd6fa2a38c5e38020df2f93ea546f6e3
Itay Brenner <itay@itaysoft.com>
2021-08-23 00:47:17 -0300

    Close Application when last window is closed

--- CameraController/AppDelegate.swift
+++ CameraController/AppDelegate.swift
@@ -119,4 +119,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // If we got here, it is time to quit.
         return .terminateNow
     }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
 }
