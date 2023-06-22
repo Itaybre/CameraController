5d81dd1bbba3ceac68f8e03782d80523ef1aa801
Itay <itay@itaysoft.com>
2023-06-21 22:56:40 -0300

    Fix some compiler and linter warnings

--- CameraController/UVC/Models/Controls/UVCControl.swift
+++ CameraController/UVC/Models/Controls/UVCControl.swift
@@ -31,75 +31,65 @@ class UVCControl {
     }
 
     func getDataFor(type: UVCRequestCodes, length: Int) -> Int {
-
-        guard uvcUnit >= 0 else { return 0 }
-
-        var value: Int = 0
-
-        let requestType = USBmakebmRequestType(direction: kUSBIn, type: kUSBClass, recipient: kUSBInterface)
-
-        var request: IOUSBDevRequest = IOUSBDevRequest(bmRequestType: requestType,
-                                                       bRequest: UInt8(type.rawValue),
-                                                       wValue: UInt16(uvcSelector<<8),
-                                                       wIndex: UInt16(uvcUnit<<8) | UInt16(uvcInterface),
-                                                       wLength: UInt16(length),
-                                                       pData: &value,
-                                                       wLenDone: 0)
-        if #available(macOS 12.0, *) {
-            guard
-                interface.pointee.pointee.ControlRequest(interface, 0, &request) == kIOReturnSuccess else {
-                    return 0
-            }
-        } else {
-            guard
-                interface.pointee.pointee.USBInterfaceOpenSeize(interface) == kIOReturnSuccess,
-                interface.pointee.pointee.ControlRequest(interface, 0, &request) == kIOReturnSuccess,
-                interface.pointee.pointee.USBInterfaceClose(interface) == kIOReturnSuccess else {
-                    return 0
-            }
+        do {
+            return try performRequest(type: type,
+                                      length: length)
+        } catch {
+            // Should not return 0, but working on improving this
+            return 0
         }
-
-        return value
     }
 
     func setData(value: Int, length: Int) -> Bool {
-
-        guard uvcUnit >= 0 else { return false }
-
-        var ref = value
-
-        let requestType = USBmakebmRequestType(direction: kUSBOut, type: kUSBClass, recipient: kUSBInterface)
-
-        var request: IOUSBDevRequest = IOUSBDevRequest(bmRequestType: requestType,
-                                                       bRequest: UInt8(UVCRequestCodes.setCurrent.rawValue),
-                                                       wValue: UInt16(uvcSelector<<8),
-                                                       wIndex: UInt16(uvcUnit<<8) | UInt16(uvcInterface),
-                                                       wLength: UInt16(length),
-                                                       pData: &ref,
-                                                       wLenDone: 0)
-
-        if #available(macOS 12.0, *) {
-            guard
-                interface.pointee.pointee.ControlRequest(interface, 0, &request) == kIOReturnSuccess else {
-                    return false
-            }
-        } else {
-            guard
-                interface.pointee.pointee.USBInterfaceOpenSeize(interface) == kIOReturnSuccess,
-                interface.pointee.pointee.ControlRequest(interface, 0, &request) == kIOReturnSuccess,
-                interface.pointee.pointee.USBInterfaceClose(interface) == kIOReturnSuccess else {
-                    return false
-            }
+        do {
+            _ = try performRequest(type: UVCRequestCodes.setCurrent,
+                               length: length,
+                               value: value)
+            return true
+        } catch {
+            return false
         }
-
-        return true
     }
 
     func updateIsCapable() {
         isCapable = getDataFor(type: UVCRequestCodes.getInfo, length: 1) != 0
     }
 
-    func USBmakebmRequestType(direction: Int, type: Int, recipient: Int) -> UInt8 {
+    private func performRequest(type: UVCRequestCodes, length: Int, value: Int = 0) throws -> Int {
+        guard uvcUnit >= 0 else {
+            throw UVCError.invalidUnitId
+        }
+
+        var value = value
+
+        let requestType = USBmakebmRequestType(direction: kUSBIn, type: kUSBClass, recipient: kUSBInterface)
+
+        try withUnsafeMutablePointer(to: &value, { value in
+            var request = IOUSBDevRequest(bmRequestType: requestType,
+                                          bRequest: UInt8(type.rawValue),
+                                          wValue: UInt16(uvcSelector<<8),
+                                          wIndex: UInt16(uvcUnit<<8) | UInt16(uvcInterface),
+                                          wLength: UInt16(length),
+                                          pData: value,
+                                          wLenDone: 0)
+            if #available(macOS 12.0, *) {
+                guard
+                    interface.pointee.pointee.ControlRequest(interface, 0, &request) == kIOReturnSuccess else {
+                    throw UVCError.requestError
+                }
+            } else {
+                guard
+                    interface.pointee.pointee.USBInterfaceOpenSeize(interface) == kIOReturnSuccess,
+                    interface.pointee.pointee.ControlRequest(interface, 0, &request) == kIOReturnSuccess,
+                    interface.pointee.pointee.USBInterfaceClose(interface) == kIOReturnSuccess else {
+                    throw UVCError.requestError
+                }
+            }
+        })
+        return value
+    }
+
+    private func USBmakebmRequestType(direction: Int, type: Int, recipient: Int) -> UInt8 {
         return UInt8((direction & kUSBRqDirnMask) << kUSBRqDirnShift) |
             UInt8((type & kUSBRqTypeMask) << kUSBRqTypeShift)|UInt8(recipient & kUSBRqRecipientMask)
 
