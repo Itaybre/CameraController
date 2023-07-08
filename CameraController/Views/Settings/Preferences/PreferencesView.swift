2bf711697af2f68297ac70a3138a0abe001cbecd
Itay <itay@itaysoft.com>
2023-07-08 01:00:18 -0300

    Redesigned PreferencesView

--- CameraController/Views/Settings/Preferences/PreferencesView.swift
+++ CameraController/Views/Settings/Preferences/PreferencesView.swift
@@ -9,82 +9,140 @@
 import SwiftUI
 
 struct PreferencesView: View {
+    @ObservedObject var manager = DevicesManager.shared
     @ObservedObject var settings = UserSettings.shared
 
     var body: some View {
         VStack(alignment: .leading) {
             applicationSettings()
-            readSettings()
-            writeSettings()
+            cameraSettings()
+            readWriteSettings()
             updatesSettings()
+            quitButton()
         }
     }
 
+    @ViewBuilder
     private func applicationSettings() -> some View {
-        return GroupBox(label: Text("Application")) {
-            HStack {
-                VStack(alignment: .leading) {
-                    Toggle(isOn: $settings.openAtLogin) {
-                        Text("Open at login")
-                    }
-                }
+        SectionView {
+            SectionTitle(title: "Application",
+                         image: Image(systemName: "flag"))
+
+            HStack(spacing: 20.0) {
+                Text("Open at login")
                 Spacer()
+                Toggle(isOn: $settings.openAtLogin) {}
+                    .toggleStyle(SwitchToggleStyle(tint: Constants.Colors.accentColor))
             }
-            HStack {
-                VStack(alignment: .leading) {
-                    Toggle(isOn: $settings.hideCameraPreview) {
-                        Text("Hide camera preview")
-                    }
-                }
+            HStack(spacing: 20.0) {
+                Text("Preview Size")
                 Spacer()
+                Picker(selection: $settings.cameraPreviewSize, label: Text("")) {
+                    Text("Disabled").tag(PreviewSizeSettings.disabled)
+                    Text("Small").tag(PreviewSizeSettings.small)
+                    Text("Medium").tag(PreviewSizeSettings.medium)
+                    Text("Large").tag(PreviewSizeSettings.large)
+                    Text("Extra Large").tag(PreviewSizeSettings.extraLarge)
+                }
+                .frame(width: 100)
             }
         }
     }
 
-    private func readSettings() -> some View {
-        return GroupBox(label: Text("Read Settings")) {
-            HStack {
-                VStack(alignment: .leading) {
-                    Picker(selection: $settings.readRate, label: Text("")) {
-                        Text("Disabled").tag(RefreshSettingsRate.disabled)
-                        Text("Every 0.5 Seconds").tag(RefreshSettingsRate.halfSecond)
-                        Text("Every 1 Second").tag(RefreshSettingsRate.oneSecond)
-                        Text("Every 2 Second").tag(RefreshSettingsRate.twoSeconds)
+    @ViewBuilder
+    private func cameraSettings() -> some View {
+        SectionView {
+            SectionTitle(title: "Camera",
+                         image: Image(systemName: "web.camera"))
+
+            HStack(spacing: 20.0) {
+                Text("Camera")
+                Spacer()
+                Picker(selection: $manager.selectedDevice, label: Text("")) {
+                    Text("None").tag(nil as CaptureDevice?)
+                    ForEach(manager.devices, id: \.self) { device in
+                        Text(device.name).tag(device as CaptureDevice?)
                     }
                 }
-                Spacer()
             }
         }
     }
 
-    private func writeSettings() -> some View {
-        GroupBox(label: Text("Push Settings")) {
-            HStack {
-                VStack(alignment: .leading) {
-                    Picker(selection: $settings.writeRate, label: Text("")) {
-                        Text("Disabled").tag(RefreshSettingsRate.disabled)
-                        Text("Every 0.5 Seconds").tag(RefreshSettingsRate.halfSecond)
-                        Text("Every 1 Second").tag(RefreshSettingsRate.oneSecond)
-                        Text("Every 2 Second").tag(RefreshSettingsRate.twoSeconds)
-                    }
-                }
+    @ViewBuilder
+    private func readWriteSettings() -> some View {
+        SectionView {
+            SectionTitle(title: "Read / Write settings from device",
+                         image: Image(systemName: "arrow.left.arrow.right"))
+
+            HStack(spacing: 20.0) {
+                Text("Read")
+                    .help("CameraController will read the configuration from the camera every X amount of time.")
                 Spacer()
+                Picker(selection: $settings.readRate, label: Text("")) {
+                    Text("Disabled").tag(RefreshSettingsRate.disabled)
+                    Text("Every 0.5 Seconds").tag(RefreshSettingsRate.halfSecond)
+                    Text("Every 1 Second").tag(RefreshSettingsRate.oneSecond)
+                    Text("Every 2 Second").tag(RefreshSettingsRate.twoSeconds)
+                }.frame(width: 100)
+            }
+            HStack(spacing: 20.0) {
+                Text("Write")
+                    .help("CameraController will write the configuration to the camera every X amount of time.")
+                Spacer()
+                Picker(selection: $settings.writeRate, label: Text("")) {
+                    Text("Disabled").tag(RefreshSettingsRate.disabled)
+                    Text("Every 0.5 Seconds").tag(RefreshSettingsRate.halfSecond)
+                    Text("Every 1 Second").tag(RefreshSettingsRate.oneSecond)
+                    Text("Every 2 Second").tag(RefreshSettingsRate.twoSeconds)
+                }.frame(width: 100)
             }
         }
     }
 
+    @ViewBuilder
     private func updatesSettings() -> some View {
+        SectionView {
+            SectionTitle(title: "Updates",
+                         image: Image(systemName: "icloud"))
+
+            HStack(spacing: 20.0) {
+                Text("Check For Updates On Startup")
+                Spacer()
+                Toggle(isOn: $settings.checkForUpdatesOnStartup) {}
+                    .toggleStyle(SwitchToggleStyle(tint: Constants.Colors.accentColor))
+            }
+            HStack(spacing: 20.0) {
+                Spacer()
+                Button("Check For Updates Now") {
+                    guard let delegate = NSApplication.shared.delegate as? AppDelegate else {
+                        return
+                    }
+                    delegate.checkForUpdates()
+                }
+                .buttonStyle(.plain)
+                .padding(.vertical, 5)
+                .padding(.horizontal, 10)
+                .background(Constants.Colors.sliderBackground)
+                .clipShape(Capsule())
+                Spacer()
+            }
+        }
+    }
+
+    @ViewBuilder
+    private func quitButton() -> some View {
         HStack {
             Spacer()
-            Button("Check for updates") {
-                guard let delegate = NSApplication.shared.delegate as? AppDelegate else {
-                    return
-                }
-                delegate.checkForUpdates()
-            }
+
             Button("Quit") {
                 NSApplication.shared.terminate(nil)
             }
+            .buttonStyle(.plain)
+            .padding(.vertical, 5)
+            .padding(.horizontal, 10)
+            .background(Constants.Colors.sliderBackground)
+            .clipShape(Capsule())
+
             Spacer()
         }
     }
