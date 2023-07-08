869066585d28fcec73af94533fdfa03bcd78450b
Itay <itay@itaysoft.com>
2023-07-08 00:53:48 -0300

    Moved some classes to groups

--- CameraController/Views/Preview/CameraPreviewInternal.swift
+++ CameraController/Views/Preview/CameraPreviewInternal.swift
@@ -22,15 +22,18 @@ final class CameraPreviewInternal: NSView {
 
         super.init(frame: frameRect)
 
-        configureDevice(device)
         setupPreviewLayer(captureSession)
-        // lock configuration to keep device.activeFormat
-        do {
-            try captureDevice?.lockForConfiguration()
-            captureSession.startRunning()
-            captureDevice?.unlockForConfiguration()
-        } catch {
-            // Handle error.
+
+        Task {
+            configureDevice(device)
+            // lock configuration to keep device.activeFormat
+            do {
+                try captureDevice?.lockForConfiguration()
+                captureSession.startRunning()
+                captureDevice?.unlockForConfiguration()
+            } catch {
+                // Handle error.
+            }
         }
 
         NotificationCenter.default.addObserver(self,
@@ -45,14 +48,19 @@ final class CameraPreviewInternal: NSView {
 
     private func setupPreviewLayer(_ captureSession: AVCaptureSession) {
         previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-        previewLayer.frame = CGRect(x: 0, y: 0, width: 400, height: 225)
+        previewLayer.frame = CGRect(
+            x: 0,
+            y: 0,
+            width: UserSettings.shared.cameraPreviewSize.getWidth(),
+            height: UserSettings.shared.cameraPreviewSize.getHeight()
+        )
         previewLayer.videoGravity = .resizeAspect
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     deinit {
         NotificationCenter.default.removeObserver(self)
     }
@@ -72,14 +80,17 @@ final class CameraPreviewInternal: NSView {
     func updateCamera(_ cam: AVCaptureDevice?) {
         if captureDevice != cam {
             captureSession.stopRunning()
-            configureDevice(cam)
-            // lock configuration to keep device.activeFormat
-            do {
-                try captureDevice?.lockForConfiguration()
-                captureSession.startRunning()
-                captureDevice?.unlockForConfiguration()
-            } catch {
-                // Handle error.
+
+            Task {
+                configureDevice(cam)
+                // lock configuration to keep device.activeFormat
+                do {
+                    try captureDevice?.lockForConfiguration()
+                    captureSession.startRunning()
+                    captureDevice?.unlockForConfiguration()
+                } catch {
+                    // Handle error.
+                }
             }
         }
     }
@@ -119,5 +130,42 @@ final class CameraPreviewInternal: NSView {
         if !captureSession.isRunning {
             captureSession.startRunning()
         }
+    }
+}
+
+extension CameraPreviewInternal {
+    override public func mouseDown(with event: NSEvent) {
+        super.mouseDown(with: event)
+
+        window?.performDrag(with: event)
+        NSCursor.contextualMenu.set()
+    }
+
+    override public func mouseEntered(with event: NSEvent) {
+        super.mouseEntered(with: event)
+
+        NSCursor.contextualMenu.set()
+    }
+
+    override public func mouseExited(with event: NSEvent) {
+        super.mouseExited(with: event)
+
+        NSCursor.arrow.set()
+    }
+
+    override func mouseMoved(with event: NSEvent) {
+        NSCursor.contextualMenu.set()
+    }
+
+    override func updateTrackingAreas() {
+        super.updateTrackingAreas()
+
+        for trackingArea in self.trackingAreas {
+            self.removeTrackingArea(trackingArea)
+        }
+
+        let options: NSTrackingArea.Options = [.mouseEnteredAndExited, .activeAlways, .mouseMoved]
+        let trackingArea = NSTrackingArea(rect: self.bounds, options: options, owner: self, userInfo: nil)
+        self.addTrackingArea(trackingArea)
     }
 }
